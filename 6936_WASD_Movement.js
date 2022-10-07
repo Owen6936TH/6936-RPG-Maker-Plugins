81d39ba662a8440920375d18aa61c815a2a5eddf
Owen6936TH <74977423+Owen6936TH@users.noreply.github.com>
2022-10-08 04:18:57 +0700

    Add files via upload

---
+++ 6936_WASD_Movement.js
@@ -0,0 +1,39 @@
+/*:
+ * @target MZ
+ * @plugindesc [Version 1.0] [RPG Maker MZ] Make your game support WASD Movement Control
+ * @author Owen6936
+ * @url https://store.steampowered.com/search/?developer=Owen6936
+ * @help
+ *
+ * English Below
+ * ====================
+ * TH
+ * ====================
+  * ----- แนะนำปลั๊กอิน -----
+ * ปลั๊กอินนี้จะทำให้เกมของท่านรองรับการเคลื่อนที่ด้วยปุ่ม WASD
+ *
+  * ----- ข้อที่ควรรู้ -----
+ * ปลั๊กอินนี้เป็นเวอร์ชั่นที่ใช้ได้ทั้ง RPG Maker MV และ RPG Maker MZ
+ * ท่านสามารถดาวน์โหลดปลั๊กอินอื่น ๆ ได้ที่ https://github.com/Owen6936TH/6936-RPG-Maker-Plugins
+ *
+ * หากท่านนำปลั๊กอินนี้ไปใช้ ขอความกรุณาให้เครดิตผู้จัดทำปลั๊กอินนี้ในเกมของท่านด้วย
+ * ขอบพระคุณเป็นอย่างสูง
+ *
+ * ====================
+ * EN
+ * ====================
+ * ----- Introduction -----
+ * This plugin will make your game support WASD Movement Control.
+ *
+ * -----  Things you should know -----
+ * This plugin is works for both RPG Maker MV and RPG Maker MZ.
+ * You can download other plugins at https://github.com/Owen6936TH/6936-RPG-Maker-Plugins
+ *
+ * Please give credit to the creators of this plugin in your game.
+ * Thank you very much
+ *
+ */
+Input.keyMapper[87] = "up"
+Input.keyMapper[65] = "left"
+Input.keyMapper[83] = "down"
+Input.keyMapper[68] = "right"
